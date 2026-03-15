f1a55872e87b39585b083f5dd197e49078324dbb
Rpinto30 <106196765+Rpinto30@users.noreply.github.com>
2026-03-14 23:33:18 -0600

    Se la lavan, hice un vrgo de cambios en como voy a estructurar esta mierda

---
+++ Minimarket/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+struct Node{
+    T data;
+    struct Node* next;
+    struct Node* prev;
+};
+
+
+int main()
+{
+    return 0;
+}
